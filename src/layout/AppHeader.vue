110673965e6ccb2d7eb449229aaaf5f21dcc2f2e
SoIidarity <christopher.maree@gmail.com>
2018-10-27 14:19:04 +0200

    Updated header navigation shortcuts

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,15 +1,15 @@
 <template>
     <header class="header-global">
         <base-nav class="navbar-main" transparent type="" effect="light" expand>
-            <a slot="brand" class="navbar-brand mr-lg-5" href="https://demos.creative-tim.com/vue-argon-design-system/documentation">
+            <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
                 <img src="img/brand/white.png">
-            </a>
+            </router-link>
 
             <div class="row" slot="content-header" slot-scope="{closeMenu}">
                 <div class="col-6 collapse-brand">
-                    <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
+                    <router-link to="/">
                         <img src="img/brand/blue.png">
-                    </a>
+                    </router-link>
                 </div>
                 <div class="col-6 collapse-close">
                     <close-button @click="closeMenu"></close-button>
