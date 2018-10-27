bf72dfd0aea73c1c8d96d579dade09bdb78e74e4
SoIidarity <christopher.maree@gmail.com>
2018-10-27 22:36:12 +0200

    Added history table and spend icons

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -48,16 +48,39 @@
                                 <span class="font-weight-light">, 27</span>
                             </h3>
                             <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Johannesburg South Africa</div>
-                            <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer</div>
-                            <div><i class="ni education_hat mr-2"></i>University of Computer Science</div>
+                            
                         </div>
-                        <div class="mt-5 py-5 border-top text-center">
-                            <div class="row justify-content-center">
-                                <div class="col-lg-9">
-                                    <p>An artist of considerable range, Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.</p>
-                                    <a href="#">Show more</a>
-                                </div>
-                            </div>
+                        <div class="mt-5  border-top text-center">
+                           <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Program</th>
+                                <th>Action</th>
+                                <th>Amount</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>2018-07-24</td>
+                                <td>Lola</td>
+                                <td>Completed A survey</td>
+                                <td>200</td>
+                            </tr>
+                            <tr>
+                                <td>2018-07-28</td>
+                                <td>Lola</td>
+                                <td>Completed A survey</td>
+                                <td>150</td>
+                            </tr>
+                            <tr>
+                                <td>2018-08-30</td>
+                                <td>Lola</td>
+                                <td>Completed A survey</td>
+                                <td>350</td>
+                            </tr>
+                            </tbody>
+                        </table>
                         </div>
                     </div>
                 </card>
@@ -155,17 +178,32 @@
                     </base-button>
                 </template>
     </modal>
-
-        <!-- MODALS -->
     <!-- Spend Window -->
     <modal :show.sync="modals.spend">
     <h4 slot="header" class="modal-title" id="modal-title-default">Spend your Ribbon Tokens</h4>
+        <div class="row">
+            <div class="col-4">
+                <img v-lazy="'img/theme/team-2-800x800.jpg'" class="" style="width:135px; cursor: pointer" @click="photoClick('woolworth')">
+            </div>
+            <div class="col-4">
+                <img v-lazy="'img/theme/team-2-800x800.jpg'" class="" style="width:135px; cursor: pointer" @click="photoClick('woolworth')">
+            </div>
+            <div class="col-4">
+                <img v-lazy="'img/theme/team-2-800x800.jpg'" class="" style="width:135px; cursor: pointer" @click="photoClick('woolworth')">
+            </div>
+        </div>
+        <div class="row pt-4">
+            <div class="col-4">
+                <img v-lazy="'img/theme/team-2-800x800.jpg'" class="" style="width:135px; cursor: pointer" @click="photoClick('woolworth')">
+            </div>
+            <div class="col-4">
+                <img v-lazy="'img/theme/team-2-800x800.jpg'" class="" style="width:135px; cursor: pointer" @click="photoClick('woolworth')">
+            </div>
+            <div class="col-4">
+                <img v-lazy="'img/theme/team-2-800x800.jpg'" class="" style="width:135px; cursor: pointer" @click="photoClick('woolworth')">
+            </div>
+        </div>
         
-        <img v-lazy="'img/theme/team-2-800x800.jpg'" class="" style="width:200px" @click="photoClick('woolworth')">
-       
-       
-    
-
     </modal>
     </div>
 </template>
@@ -193,8 +231,8 @@ export default {
     setMaxWithdraw() {
       this.withdrawValue = this.walletBalance;
     },
-    contactSelect(address){
-        this.withdrawAddress = address
+    contactSelect(address) {
+      this.withdrawAddress = address;
     },
     photoClick(companyName) {
       console.log(companyName);
