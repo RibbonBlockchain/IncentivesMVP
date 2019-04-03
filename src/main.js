b8709ef9b2be6a47f5a439c7dc8eb7434a7e1f20
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-04-03 20:26:49 +0100

    Configure project

--- src/main.js
+++ src/main.js
@@ -3,20 +3,26 @@ require("dotenv").config();
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from './store'
-import Argon from "./plugins/argon-kit";
-import Provider from "./graphql";
 import Notifications from "vue-notification";
+// import store from './store'
+import Argon from "./plugins/argon-kit";
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+import aws_exports from "./aws-exports";
+
 Vue.use(Notifications);
 var VueScrollTo = require("vue-scrollto");
 
 Vue.use(VueScrollTo);
 Vue.config.productionTip = false;
+
+Amplify.configure(aws_exports);
+
+Vue.use(AmplifyPlugin, AmplifyModules);
 Vue.use(Argon);
 
 new Vue({
   router,
-  render: h => h(App),
-  provide: Provider.provide(),
-  store,
+  render: h => h(App)
+  // store,
 }).$mount("#app");
